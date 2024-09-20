7abf57b2e1e23b89c8fa326393034a2b89933151
23nhannt <nguyentiennhan9a2@gmail.com>
2024-09-20 09:45:40 +0700

    update 20/9/2024

--- download_script.py
+++ download_script.py
@@ -4,4 +4,4 @@ tickers = ['MCD', 'SBUX', 'YUM', 'QSR', 'DPZ', 'LKNCY', 'DNUT', 'BRK-A', 'WEN', 
 
 for ticker in tickers:
     data = yf.download(ticker)
-    data.to_csv(f'10-largest-fast-food-chains-stock/{ticker}.csv')
+    data.to_csv(f'10-Largest-Fast-Food-Companies-Stock/stocks/{ticker}.csv')
